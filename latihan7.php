0fdf138d2f070c98be078e2933713d200fbbdae3
dita-tianto <90166743+dita-tianto@users.noreply.github.com>
2021-12-07 11:33:37 +0700

    Add files via upload

---
+++ latihan7.php
@@ -0,0 +1,15 @@
+<?php
+
+$arrBuah = array ("mangga", "apel", "pisang", "jeruk");
+echo $arrBuah [0]; //mangga
+echo $arrBuah [3]; //jeruk
+
+$arrWarna = array();
+$arrwarna[] = "Merah";
+$arrWarna[] = "Biru";
+$arrwarna[] = "Hijau";
+$arrwarna[] = "Putih";
+echo $arrWarna[0]; //Merah
+echo $arrWarna[2]; //Hijau
+
+?>
